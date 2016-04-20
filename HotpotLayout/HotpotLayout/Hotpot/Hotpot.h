3e889bbe6aa8f86b458d33c2608d839fafff8903
meterscao <meterscao@tencent.com>
2016-04-20 17:17:01 +0800

    Add message

--- HotpotLayout/HotpotLayout/Hotpot/Hotpot.h
+++ HotpotLayout/HotpotLayout/Hotpot/Hotpot.h
@@ -1,6 +1,7 @@
 //
 //  Hotpot.h
 //  HotpotLayout
+//  HomePage: https://github.com/hellometers/Hotpot
 //
 //  Created by meterscao on 16/4/19.
 //  Copyright © 2016年 meterscao. All rights reserved.
