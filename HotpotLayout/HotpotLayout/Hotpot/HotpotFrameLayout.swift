75f4da7e4913d04c3929ed3e469241d04af7a358
R0uter <lomyr.jo@gmail.com>
2018-04-01 18:30:12 +0800

    first commit!

---
+++ HotpotLayout/HotpotLayout/Hotpot/HotpotFrameLayout.swift
@@ -0,0 +1,9 @@
+//
+//  HotpotFrameLayout.swift
+//  HotpotLayout
+//
+//  Created by R0uter on 2018/4/1.
+//  Copyright © 2018 R0uter. All rights reserved.
+//
+
+import Foundation
---
+++ HotpotLayout/HotpotLayout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HotpotLayout
+//
+//  Created by R0uter on 2018/4/1.
+//  Copyright © 2018 R0uter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
